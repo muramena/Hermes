ca39f2ef760a9b59f8aa268d45313969dc6e6022
Manuel Garcia Genta <man.mdp@gmail.com>
2019-10-13 17:33:20 -0300

    Router

---
+++ server/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express')
+var app = express()
+const path = require('path')
+
+app.get('/usuarios', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../public/views/user/index.html'));
+})
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const socketIO = require('socket.io');
-const http = require('http');
+const express = require('express')
+const http = require('http')
 
-const path = require('path');
+const path = require('path')
 
-const app = express();
-let server = http.createServer(app);
+const app = express()
+let server = http.createServer(app)
 
-const publicPath = path.resolve(__dirname, '../public');
+const publicPath = path.resolve(__dirname, 'public')
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath))
 
-module.exports.io = socketIO(server);
-require('./sockets/socket');
+app.use(require('./routes'))
 
 server.listen(port, (err) => {
-
     if (err) throw new Error(err);
-
     console.log(`Servidor corriendo en puerto ${ port }`);
-
-});
+})
