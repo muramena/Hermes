92f6465712ef17adc52f1f6c833ea77e0acc713e
Manuel Garcia Genta <man.mdp@gmail.com>
2019-10-13 17:53:40 -0300

    Folder structure

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ var app = express()
 const path = require('path')
 
 app.get('/usuarios', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../public/views/user/index.html'));
+  res.sendFile(path.resolve(__dirname, '../../public/views/user/index.html'));
 })
 
 module.exports = app
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path')
 const app = express()
 let server = http.createServer(app)
 
-const publicPath = path.resolve(__dirname, 'public')
+const publicPath = path.resolve(__dirname, '../public')
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath))
