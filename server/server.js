f11136fbbf1929f2f304f91b0482ced72a142628
Manuel Garcia Genta <man.mdp@gmail.com>
2019-10-14 21:27:07 -0300

    Add sass-middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const http = require('http')
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
+const sassMiddleware = require('node-sass-middleware')
 const path = require('path')
 
 const app = express()
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,17 +18,26 @@ let server = http.createServer(app)
 const publicPath = path.resolve(__dirname, '../public')
 const port = process.env.PORT || 3000;
 
+// sass config
+app.use(
+  sassMiddleware({
+    src: path.resolve(__dirname, '../sass'),
+    dest: publicPath,
+    debug: true
+  })
+);   
+
 app.use(express.static(publicPath))
 
 app.use(require('./routes'))
 
 // BASE DE DATOS, DESACTIVAR SI NO ESTA INSTALADO MONGO
 mongoose.connect("mongodb://localhost:27017/Hermes", { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos ONLINE');
+  if (err) throw err;
+  console.log('Base de datos ONLINE');
 });
 
 server.listen(port, (err) => {
-    if (err) throw new Error(err);
-    console.log(`Servidor corriendo en puerto ${ port }`);
+  if (err) throw new Error(err);
+  console.log(`Servidor corriendo en puerto ${ port }`);
 })
