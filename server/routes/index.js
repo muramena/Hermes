9d562e36e43899a2f16c626ef82d55ec5afeb3c9
Florencio <florovarelaa@gmail.com>
2019-10-21 11:45:29 -0300

    Worked on endpoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 var app = express();
 const user = require('./user');
 const ticket = require('./ticket');
+const views = require('./views');
 
+app.use(views);
 app.use(user);
 app.use(ticket);
 
